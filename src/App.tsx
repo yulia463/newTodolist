b16becc25c30e7d1b83a895e8899df243cdbfc59
yuliaslatvitskaya <yuli.slat@yandex.ru>
2022-10-13 19:16:14 +0200

    Input and button

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,22 @@ import './App.css';
 import Todolist from "./Todolist";
 
 function App() {
-    let tasks =
+    const tasks1 =
         [
             {id: 1, title: 'Egg', isDone: true},
             {id: 2, title: 'Bacon', isDone: true},
             {id: 3, title: 'Burger', isDone: false}
         ]
+    const tasks2 =
+        [
+            {id: 1, title: 'Ball', isDone: true},
+            {id: 2, title: 'sneaker', isDone: false},
+            {id: 3, title: 'rocket', isDone: false}
+        ]
     return (
         <div className="App">
-            {/*<Todolist title={} tasks={}/>*/}
+            <Todolist title={'Consumer basket'} tasks={tasks1}/>
+            <Todolist title={'Sport goods'} tasks={tasks2}/>
         </div>
     );
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -5,12 +5,36 @@ export type TodoListPropsType = {
     tasks: Array<TaskType>
 }
 export type TaskType = {
-    id: string
+    id: number
     title: string
     isDone: boolean
 }
 
 function TodoList(props: TodoListPropsType) {
+
+
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {props.tasks.map((task)=>{
+                    return(
+                        <li key={task.id}><input type={'checkbox'} checked={task.isDone}/><span>{task.title}</span></li>
+                    )
+                })}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+
+        </div>
+    )
 }
 
 export default TodoList;
