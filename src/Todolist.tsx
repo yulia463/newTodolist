88b070fcb47b537e576c1d9997dbda691003aeeb
yuliaslatvitskaya <yuli.slat@yandex.ru>
2022-10-17 15:19:36 +0200

    add: removeTask and changeFilter

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import {FilterValuesType} from "./App";
+
 
 export type TodoListPropsType = {
     title: string
     tasks: Array<TaskType>
+    removeTask: (taskId: number) => void
+    changeFilter:(value:FilterValuesType)=>void
 }
 export type TaskType = {
     id: number
@@ -21,14 +25,33 @@ function TodoList(props: TodoListPropsType) {
             <ul>
                 {props.tasks.map((task) => {
                     return (
-                        <li key={task.id}><input type={'checkbox'} checked={task.isDone}/><span>{task.title}</span></li>
+                        <li key={task.id}>
+                            <input type={'checkbox'} checked={task.isDone}/>
+                            <span>{task.title}</span>
+                            <button onClick={() => {
+                                props.removeTask(task.id)
+                            }}>
+                                ✖
+                                ️
+                            </button>
+                        </li>
                     )
                 })}
             </ul>
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <button onClick={() => {
+                    props.changeFilter('all')
+                }}>All
+                </button>
+                <button onClick={() => {
+                    props.changeFilter('active')
+                }}>
+                    Active
+                </button>
+                <button onClick={() => {
+                    props.changeFilter('completed')
+                }}>Completed
+                </button>
             </div>
 
         </div>
